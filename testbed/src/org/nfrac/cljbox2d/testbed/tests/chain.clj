2951dbefdb23cae1274f92ef50ffa682670c9c88
Felix Andrews <felix@nfrac.org>
2015-01-01 13:51:05 +0800

    chain test

---
+++ testbed/src/org/nfrac/cljbox2d/testbed/tests/chain.clj
@@ -0,0 +1,52 @@
+(ns org.nfrac.cljbox2d.testbed.tests.chain
+  "A translation of Daniel Murphy's
+   org.jbox2d.testbed.tests.Chain"
+  (:require [org.nfrac.cljbox2d.testbed :as bed]
+            [cljbox2d.core :refer :all]
+            [cljbox2d.joints :refer :all]
+            [quil.core :as quil]
+            [quil.middleware]))
+
+(defn setup []
+  (quil/frame-rate 30)
+  (let [world (new-world)
+        ground (body! world {:type :static}
+                      {:shape (edge [-40 0] [40 0])})
+        link-fd {:shape (box 0.6 0.125)
+                  :density 20
+                  :friction 0.2}
+        y 25.0
+        links (reduce (fn [prev-body i]
+                        (let [b (body! world {:position [(+ 0.5 i) y]}
+                                       link-fd)]
+                          (joint! {:type :revolute
+                                   :body-a prev-body
+                                   :body-b b
+                                   :anchor [i y]
+                                   :collide-connected false})
+                          b))
+                      ground
+                      (range 30))]
+    (assoc bed/initial-state
+      :world world)))
+
+(defn step
+  [state]
+  (if (:paused? state)
+    state
+    (update-in state [:world] step! (:dt-secs state))))
+
+(defn -main
+  "Run the test sketch."
+  [& args]
+  (quil/defsketch test-sketch
+    :title "Chain"
+    :setup setup
+    :update step
+    :draw bed/draw
+    :key-typed bed/key-press
+    :mouse-pressed bed/mouse-pressed
+    :mouse-released bed/mouse-released
+    :mouse-dragged bed/mouse-dragged
+    :size [600 500]
+    :middleware [quil.middleware/fun-mode]))
